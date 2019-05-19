2ad3cbb0c90d57f0d678e764b079cb20332ca66d
John <baran.j@hotmail.com>
2019-05-19 17:40:29 -0400

    testing.sh updated with a UI, can select which files to add or remove permissions to

--- testing.sh
+++ testing.sh
@@ -1,11 +1,119 @@
-ls -l > files_to_change.txt
+ls -l ~/Desktop/> files_to_change.txt
 
 awk {'print $9'} files_to_change.txt > the_files.txt
 
-for i in 2 3 4 5
+max=$(wc -l < the_files.txt)
+
+cd ..
+echo -e "____________________________\nPermissions before:\n"
+ls -l
+echo "____________________________"
+for ((i=2; i<=$max; i++))
 do
 
-file=$(cat the_files.txt | head -n "${i}" | tail -n 1)
+file=$(cat ~/Desktop/Permissions-Changer/the_files.txt | head -n "${i}" | tail -n 1)
 echo $file
-chmod u-x $file
+if [ "$file" != "Permissions-Changer" ]; then
+
+echo -e "Would you like to edit the permissions for the file: $file\n Yes(y), No(n)"
+while :
+do
+read edit
+case $edit in
+	y)
+
+while :
+do
+echo -e "Choose an option\n1) User\n2) Group\n3) Other:\n"
+read user
+case $user in 
+	1)
+	user="u"
+	break
+	;;
+
+	2)
+	user="g"
+	break
+	;;
+
+	3)
+	user="o"
+	break
+	;;
+
+	*)
+	echo -e "Please choose either 1, 2 or 3 as an option\n"
+	;;
+esac
 done
+
+
+while :
+do
+echo -e  "Choose to\n1) Add\n2) Remove\nthe permission:\n"
+read perm
+case $perm in 
+	1)
+	perm="+"
+	break
+	;;
+
+	2)
+	perm="-"
+	break
+	;;
+
+	*)
+	echo -e "Please choose either 1 or 2 as an option\n"
+	;;
+esac
+done
+
+while :
+do
+echo -e  "Choose\n1) Read\n2) Write\n3) Executable:\n"
+read type
+case $type in 
+	1)
+	type="r"
+	break
+	;;
+
+	2)
+	type="w"
+	break
+	;;
+
+	3)
+	type="x"
+	break
+	;;
+
+	*)
+	echo -e "Please choose either 1, 2 or 3 as an option\n"
+	;;
+esac
+done
+
+	chmod $user$perm$type $file
+	echo "changed the permissions on" $file
+	break
+	;;
+
+	n)
+	echo "No changes made to $file"
+	break
+	;;
+
+	*)
+	echo "Enter a valid option Yes(y) or No(n)"
+esac
+done
+else
+:
+fi
+done
+echo -e "_________________________________________\nPermissions after:"
+ls -l
+echo "_________________________________________"
